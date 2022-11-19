d7fa5dbccb269073833861d1085ad95ac2327fba
socheathorl <socheathorl173@gmail.com>
2022-11-19 09:49:36 +0700

    redirect to docs

--- server/api.py
+++ server/api.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from server.routes import router as NoteRouter
 
 app = FastAPI()
 
 @app.get("/", tags=["Root"])
 async def read_root():
-  return {
-    "message": "Welcome to my note application, use the /docs route to proceed"
-  }
+  return RedirectResponse("/docs")
+  # return {
+  #   "message": "Welcome to my note application, use the /docs route to proceed"
+  # }
 
 
 app.include_router(NoteRouter, prefix="/note")
